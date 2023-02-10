12ff646d2a0b3e063ce136ccfef8020b925c41a9
BuildTools <unconfigured@null.spigotmc.org>
2023-02-10 04:02:48 +0100

    server src

--- drone server/drone server/Constants.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define MAGIC_STRING "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-
-enum ClientType 
-{
-	WebSocket,
-	RawSocket
-};
--- drone server/drone server/Listeners.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class ClientDataListener
-{
-public:
-	virtual void onData(unsigned char* data, int lnt) {};
-	virtual void onCommandData(unsigned char* data, int lnt) {};
-};
